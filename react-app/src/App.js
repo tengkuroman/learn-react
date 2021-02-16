c8781ec1340f9f8ad59eb95351286021066ef994
Tengku Romansyah <tengku.romansyah@gmail.com>
2021-02-16 20:50:18 +0700

    Render new component to main page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import About from './page-1/About';
+import { About } from './page-1/About';
+import FruitList from './page-1/FruitList';
+
+let dataHargaBuah = [
+  { nama: 'Semangka', harga: 10000, berat: 1000 },
+  { nama: 'Anggur', harga: 40000, berat: 500 },
+  { nama: 'Strawberry', harga: 30000, berat: 400 },
+  { nama: 'Jeruk', harga: 30000, berat: 1000 },
+  { nama: 'Mangga', harga: 30000, berat: 500 },
+];
 
 function App() {
-  return <About />;
+  return (
+    <div>
+      <About />
+      <FruitList data={dataHargaBuah} />
+    </div>
+  );
 }
 
 export default App;
